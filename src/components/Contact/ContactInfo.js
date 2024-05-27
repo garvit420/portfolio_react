7ddd3970996914485d0f789b61c8011bfe71947b
garv420 <garvit.23bcs10091@ms.sst.scaler.com>
2024-05-27 19:48:32 +0530

    added contact

---
+++ src/components/Contact/ContactInfo.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './Contact.module.css';
+
+const ContactInfo = () => {
+  return (
+    <div className={styles.contactInfo}>
+      <h2>Available for select freelance opportunities</h2>
+      <p>Have an exciting project you need help with? <br></br>Send me an email or contact me via instant message!</p>
+      <a href="mailto:tamal@tamalsen.dev" className={styles.email}>tamal@tamalsen.dev</a>
+      <div className={styles.socialLinks}>
+        <a href="https://messenger.com" target="_blank" rel="noopener noreferrer">Messenger</a>
+        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
+        <a href="https://github.com" target="_blank" rel="noopener noreferrer">Github</a>
+      </div>
+    </div>
+  );
+};
+
+export default ContactInfo;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,11 +8,11 @@ const Header = () => {
         <div className="name-logo">GarvitJain._.</div>
         <nav className='navbar'>
           <ul className='nav-list'>
-            <li className='nav-item' data-number='01'><a href="#home">// Home</a></li>
-            <li className='nav-item' data-number='02'><a href="#expertise">// Expertise</a></li>
-            <li className='nav-item' data-number='03'><a href="#work">// Work</a></li>
-            <li className='nav-item' data-number='04'><a href="#experience">// Experience</a></li>
-            <li className='nav-item' data-number='05'><a href="#contact">// Contact</a></li>
+            <li className='nav-item' data-number='01'><a href="#home">{"// Home"}</a></li>
+            <li className='nav-item' data-number='02'><a href="#expertise">{"// Expertise"}</a></li>
+            <li className='nav-item' data-number='03'><a href="#work">{"// Work"}</a></li>
+            <li className='nav-item' data-number='04'><a href="#experience">{"// Experience"}</a></li>
+            <li className='nav-item' data-number='05'><a href="#contact">{"// Contact"}</a></li>
           </ul>
         </nav>
       </div>
